4565285d9edc9f5c64a846ffc5212cb751f493b3
ig <ig@pss.one>
2019-07-22 19:38:49 +0300

    Add Start Activity

--- app/src/main/java/com/example/chat/MainActivity.java
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.chat;
 
+import android.content.Intent;
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -13,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            Intent startIntent = new Intent(MainActivity.this,StartActivity.class);
+            startActivity(startIntent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/chat/StartActivity.java
+++ app/src/main/java/com/example/chat/StartActivity.java
@@ -1,8 +1,9 @@
 package com.example.chat;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
 
 
 public class StartActivity extends AppCompatActivity {
