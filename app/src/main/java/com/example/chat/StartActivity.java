494a8190c573690fa0d25104eda5205c4c5e34b4
ig <ig@pss.one>
2019-07-23 15:40:36 +0300

    design registration Layout

--- app/src/main/java/com/example/chat/StartActivity.java
+++ app/src/main/java/com/example/chat/StartActivity.java
@@ -1,18 +1,31 @@
 package com.example.chat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
 
 public class StartActivity extends AppCompatActivity {
-
+    private Button mRegBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        mRegBtn =findViewById(R.id.reg_btn);
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regIntent = new Intent(StartActivity.this,RegisterActivity.class);
+                startActivity(regIntent);
+            }
+        });
 
 
     }
