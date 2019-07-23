d8680cd8998cf00a73bf79ff11a82791608cffca
ig <ig@pss.one>
2019-07-23 15:51:45 +0300

    init reg btn

--- app/src/main/java/com/example/chat/RegisterActivity.java
+++ app/src/main/java/com/example/chat/RegisterActivity.java
@@ -3,12 +3,18 @@ package com.example.chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button regDisplayName,regEmail,regPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        regDisplayName = findViewById(R.id.regDisplayName);
+        regEmail = findViewById(R.id.regEmailEdittext);
+        regPassword = findViewById(R.id.regPassword);
+
     }
 }
