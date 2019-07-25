1498e99651a916038954e383fa0e7f8f0b8faca3
ig <ig@pss.one>
2019-07-25 12:48:18 +0300

    add Firebase reg

--- app/src/main/java/com/example/chat/MainActivity.java
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -2,32 +2,92 @@ package com.example.chat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+     Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+        //Firebase Auth
+
+
+        //Layout
+        mToolbar = findViewById(R.id.mainPageToolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("My Chat Main Page");
+
+        //Layout
+
+
+
+
 
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser == null) {
-            Intent startIntent = new Intent(MainActivity.this,StartActivity.class);
-            startActivity(startIntent);
-            finish();
+        FirebaseUser curentUser = mAuth.getCurrentUser();
+        if (curentUser == null){
+            sendToStart();
         }
+        // Check if user is signed in (non-null) and update UI accordingly.
+
+    }
+
+    private void sendToStart() {
+        Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(startIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+
+         getMenuInflater().inflate(R.menu.main_menu, menu);
+
+
+
+         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+         super.onOptionsItemSelected(item);
+         if (item.getItemId() == R.id.mainLogoutBtn){
+             Log.d("Menu", "Logout");
+             if (FirebaseAuth.getInstance().getCurrentUser() != null){
+                 FirebaseAuth.getInstance().signOut();
+                 sendToStart();
+                 Log.d("Menu", "Logout null");
+             }
+
+         }
+         if (item.getItemId() == R.id.mainAllUsersBtn){
+             //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+             //Log.d("Menu", "All users "+FirebaseAuth.getInstance().getCurrentUser().getDisplayName().toString());
+         }
+         if (item.getItemId() == R.id.mainSettingsBtn){
+
+         }
+
+
+         return true;
     }
 }
--- app/src/main/java/com/example/chat/RegisterActivity.java
+++ app/src/main/java/com/example/chat/RegisterActivity.java
@@ -1,20 +1,89 @@
 package com.example.chat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
-    Button regDisplayName,regEmail,regPassword;
+    private FirebaseUser user;
+    private FirebaseAuth mAuth;
+    private Button createAccountBtn;
+    private TextInputLayout regDisplayName,regEmail,regPassword;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+        //Layout
+        //Toolbar set
+        mToolbar = findViewById(R.id.registerToolBar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Registration");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
         regDisplayName = findViewById(R.id.regDisplayName);
         regEmail = findViewById(R.id.regEmailEdittext);
         regPassword = findViewById(R.id.regPassword);
 
+        createAccountBtn = findViewById(R.id.createBtn);
+        createAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String displayName = regDisplayName.getEditText().getText().toString();
+                String email = regEmail.getEditText().getText().toString();
+                String pasword = regPassword.getEditText().getText().toString();
+                registerUser(displayName,email,pasword);
+            }
+        });
+
+    }
+
+    private void registerUser(String displayName, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Auth", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(mainIntent);
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Auth", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+
+    }
+
+    private void updateUI(FirebaseUser user) {
+        //this.user = user;
+
+        return;
     }
 }
