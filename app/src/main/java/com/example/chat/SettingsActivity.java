6df9083d1deca4fa22fb5e623be3b81af2741a19
ig <ig@pss.one>
2019-07-30 16:26:08 +0300

    add sqare aspect ratio

--- app/src/main/java/com/example/chat/SettingsActivity.java
+++ app/src/main/java/com/example/chat/SettingsActivity.java
@@ -1,13 +1,16 @@
 package com.example.chat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,6 +19,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -51,11 +56,17 @@ public class SettingsActivity extends AppCompatActivity {
         imageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
                 Intent galleryIntent = new Intent();
                 galleryIntent.setType("image/*");
                 galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
 
                 startActivityForResult(Intent.createChooser(galleryIntent, "SELECT IMAGE"),GALLERY_PICK);
+
+/*                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(SettingsActivity.this);*/
 
             }
         });
@@ -96,6 +107,24 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
         //Database
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_PICK && resultCode == RESULT_OK){
+
+
+
+            Uri imageURI = data.getData();
+
+            CropImage.activity(imageURI)
+                    .setAspectRatio(1,1)
+                    .start(this);
+
+           // Toast.makeText(SettingsActivity.this,imageURI,Toast.LENGTH_LONG).show();
+        }
 
     }
 }
