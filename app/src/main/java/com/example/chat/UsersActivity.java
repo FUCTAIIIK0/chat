91c28eac4351bb7e66bc02f77684ad005286c99d
ig <ig@pss.one>
2019-08-02 11:29:31 +0300

    fix onBack Button UsersActivity

--- app/src/main/java/com/example/chat/UsersActivity.java
+++ app/src/main/java/com/example/chat/UsersActivity.java
@@ -2,21 +2,27 @@ package com.example.chat;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 
 public class UsersActivity extends AppCompatActivity {
     private Toolbar users_toolBar;
+    private RecyclerView users_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
-        users_toolBar = findViewById(R.id.usersToolBar);
+        //Layout
+        users_toolBar = findViewById(R.id.usersAppBar);
         setSupportActionBar(users_toolBar);
         getSupportActionBar().setTitle("Users Page");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        users_list =findViewById(R.id.users_list);
 
     }
 }
