b6f36bd92104f27e52083e3fb853fb9d82fd00bc
ig <ig@pss.one>
2019-08-06 17:08:53 +0300

    add Name, onlineStatus on Chat

--- app/src/main/java/com/example/chat/ChatActivity.java
+++ app/src/main/java/com/example/chat/ChatActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,10 +20,13 @@ import com.squareup.picasso.Callback;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
 public class ChatActivity extends AppCompatActivity {
     //layout
     private Toolbar mChatToolbar;
     private ImageView chatImage;
+    private TextView name,online;
 
     //Database
     private String mChatUser;
@@ -40,14 +44,16 @@ public class ChatActivity extends AppCompatActivity {
         mRootRef.child("Users").child(mChatUser).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
+                //set Name
                 String chat_user_name = dataSnapshot.child("name").getValue().toString();
-                getSupportActionBar().setTitle(chat_user_name);
-
+                name.setText(chat_user_name);
+                //set Online
+                String chat_online = dataSnapshot.child("online").getValue().toString();
+                online.setText(chat_online);
+                //set Image
                 chatImage = findViewById(R.id.chat_userImage);
                 String thumb_image = dataSnapshot.child("thumb_image").getValue().toString();
                 Log.d("thumb_image", "onDataChange: "+thumb_image);
-
                 Picasso.with(ChatActivity.this).load(thumb_image).networkPolicy(NetworkPolicy.OFFLINE).placeholder(R.drawable.userpic).into(chatImage, new Callback() {
                     @Override
                     public void onSuccess() {
@@ -58,6 +64,7 @@ public class ChatActivity extends AppCompatActivity {
 
                     }
                 });
+                //Click on image
                 chatImage.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -66,7 +73,6 @@ public class ChatActivity extends AppCompatActivity {
                         startActivity(profiliIntent);
                     }
                 });
-
             }
 
             @Override
@@ -79,7 +85,11 @@ public class ChatActivity extends AppCompatActivity {
         mChatToolbar = findViewById(R.id.chat_appBar);
         setSupportActionBar(mChatToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(" ");
+
         chatImage = findViewById(R.id.chat_userImage);
+        name =findViewById(R.id.chat_displayName);
+        online = findViewById(R.id.chat_onlineStatus);
 
 
 
