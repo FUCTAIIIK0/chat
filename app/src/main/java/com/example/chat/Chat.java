ba1221e52bbc8aaad69042e640804946fc66a56a
ig <ig@pss.one>
2019-08-07 16:14:18 +0300

    add send message to database

--- app/src/main/java/com/example/chat/Chat.java
+++ app/src/main/java/com/example/chat/Chat.java
@@ -20,5 +20,8 @@ public class Chat extends Application {
         build.setLoggingEnabled(true);
         Picasso.setSingletonInstance(build);
 
+
+
+
     }
 }
--- app/src/main/java/com/example/chat/ChatActivity.java
+++ app/src/main/java/com/example/chat/ChatActivity.java
@@ -1,37 +1,48 @@
 package com.example.chat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
-import org.w3c.dom.Text;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
     //layout
     private Toolbar mChatToolbar;
     private ImageView chatImage;
     private TextView name,online;
+    private ImageButton chatAddBtn, chatSendBtn;
+    private EditText chatMessage;
 
     //Database
     private String mChatUser;
     private DatabaseReference mRootRef;
+    private FirebaseAuth mAth;
+    private String mCurrentUserID;
+
 
 
 
@@ -48,12 +59,18 @@ public class ChatActivity extends AppCompatActivity {
         chatImage = findViewById(R.id.chat_userImage);
         name =findViewById(R.id.chat_displayName);
         online = findViewById(R.id.chat_onlineStatus);
-
+        chatAddBtn = findViewById(R.id.chat_addBtn);
+        chatSendBtn = findViewById(R.id.chatSendBtn);
+        chatMessage = findViewById(R.id.chat_editText);
 
 
         //Databse
         mChatUser = getIntent().getStringExtra("user_id");
         mRootRef = FirebaseDatabase.getInstance().getReference();
+        mAth = FirebaseAuth.getInstance();
+        mCurrentUserID = mAth.getCurrentUser().getUid();
+
+        //getUserData
         mRootRef.child("Users").child(mChatUser).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -93,12 +110,94 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
+        //Messaging
+        mRootRef.child("Chat").child(mCurrentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (!dataSnapshot.hasChild(mChatUser)){
+
+                    Map chatAddMap = new HashMap();
+                    chatAddMap.put("seen",false);
+                    chatAddMap.put("timestamp", ServerValue.TIMESTAMP);
+
+                    Map chatUserMap = new HashMap();
+                    chatUserMap.put("Chat/"+mCurrentUserID+"/"+mChatUser, chatAddMap);
+                    chatUserMap.put("Chat/"+mChatUser+"/"+mCurrentUserID,chatAddMap);
+
+                    mRootRef.updateChildren(chatUserMap, new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                            if (databaseError !=null){
+                                Log.d("CHAT_LOG", databaseError.getMessage().toString());
+                            }
+
+                        }
+                    });
 
 
 
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        chatAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        chatSendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+
+            }
+        });
 
 
+    }
 
+    private void sendMessage() {
+        String message = chatMessage.getText().toString();
+
+
+        if (!TextUtils.isEmpty(message)){
+
+            String current_user_ref = "messages/"+mCurrentUserID+"/"+mChatUser;
+            String chat_user_ref = "messages/"+mChatUser+"/"+mCurrentUserID;
+
+            DatabaseReference user_message_push = mRootRef.child("messages").
+                    child(mCurrentUserID).child(mChatUser).push();
+
+            String push_id = user_message_push.getKey();
+
+            Map messageMap = new HashMap();
+            messageMap.put("message",message);
+            messageMap.put("seen",false);
+            messageMap.put("type","text");
+            messageMap.put("time",ServerValue.TIMESTAMP);
+
+            Map messageUserMap = new HashMap();
+            messageUserMap.put(current_user_ref+"/"+push_id, messageMap);
+            messageUserMap.put(chat_user_ref+"/"+push_id, messageMap);
+
+            mRootRef.updateChildren(messageUserMap, new DatabaseReference.CompletionListener() {
+                @Override
+                public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                    if (databaseError !=null){
+                        Log.d("CHAT_LOG", databaseError.getMessage().toString());
+                    }
+                }
+            });
+
+        }
 
     }
 }
